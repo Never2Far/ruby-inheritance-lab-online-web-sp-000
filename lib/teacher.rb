d3fc29155849fc7ec6eac038c070e20141f006a8
Seth Near <seth.j.near@gmail.com>
2020-06-28 03:02:51 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -6,9 +6,9 @@ class Teacher < User
 # def initialize(first_name, last_name)
 #
 # end
-def initialize(first, last)
-  User.new(first, last)
-end
+# def initialize(first, last)
+#   User.new(first, last)
+# end
 
 
 
