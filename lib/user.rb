689f0337338abf99f1ee11bfa4df6e94dafb6150
Seth Near <seth.j.near@gmail.com>
2020-06-28 02:58:28 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,7 @@
 class User
 
-# attr_accessor :first_name, :last_name
+
+attr_accessor :first_name, :last_name
 
 
 def initialize(first_name, last_name)
@@ -8,21 +9,21 @@ def initialize(first_name, last_name)
   @last_name = last_name
 end
 
-def first_name=(name)
-  @first_name = name
-end
-
-def first_name
-  return @first_name
-end
-
-def last_name=(name)
-  @last_name = name
-end
-
-def last_name
-  return @last_name
-end
+# def first_name=(name)
+#   @first_name = name
+# end
+# 
+# def first_name
+#   return @first_name
+# end
+# 
+# def last_name=(name)
+#   @last_name = name
+# end
+# 
+# def last_name
+#   return @last_name
+# end
 
 
 end
