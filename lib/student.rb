5676107c53047fea1d6404c34fe72ce5241cd535
Seth Near <seth.j.near@gmail.com>
2020-06-28 03:07:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,20 @@ require_relative "./user.rb"
 class Student < User
 
 def initialize
-  knowledge = []
+  @knowledge = []
 end
+
+
+def learn(string)
+  @knowledge << string
+end
+
+
+
+def knowledge
+  return @ knowledge
+end
+
+
 
 end
