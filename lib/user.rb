9d6d14a0e746d41542f0f72884c2486b40c3bc22
Seth Near <seth.j.near@gmail.com>
2020-06-28 03:08:46 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -9,21 +9,4 @@ def initialize
   @last_name = nil
 end
 
-# def first_name=(name)
-#   @first_name = name
-# end
-#
-# def first_name
-#   return @first_name
-# end
-#
-# def last_name=(name)
-#   @last_name = name
-# end
-#
-# def last_name
-#   return @last_name
-# end
-
-
 end
